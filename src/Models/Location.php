7b09a520d8967398d71eaf262785f3f2828b1034
Chitmin <aungchitmin@owaytrip.com>
2018-08-15 10:58:08 +0630

    update locations

---
+++ src/Models/Location.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace BmgApiV2Lib\Models;
+
+use JsonSerializable;
+
+class Location implements JsonSerializable
+{
+    /**
+     * @var string $continent
+     */
+    public $continent;
+
+    /**
+     * @var string $code
+     */
+    public $code;
+
+    /**
+     * @var string $uuid
+     */
+    public $uuid;
+
+    /**
+     * @var \BmgApiV2Lib\Models\Countries
+     */
+    public $countries;
+
+    /**
+     * Constructor to set initial or default values of member properties
+     * @param string $name Initialization value for $this->name
+     * @param string $code Initialization value for $this->code
+     * @param string $uuid Initialization value for $this->uuid
+     * @param string $uuid Initialization value for $this->countries
+     */
+    public function __construct()
+    {
+        if (4 == func_num_args()) {
+            $this->name = func_get_arg(0);
+            $this->code = func_get_arg(1);
+            $this->uuid = func_get_arg(2);
+            $this->countries = func_get_arg(3);
+        }
+    }
+
+
+    /**
+     * Encode this object to JSON
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        $json = [];
+        $json['name'] = $this->name;
+        $json['code'] = $this->code;
+        $json['uuid'] = $this->uuid;
+        $json['countries'] = $this->countries;
+
+        return $json;
+    }
+}
---
+++ src/Models/Locations.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BmgApiV2Lib\Models;
+
+use JsonSerializable;
+
+class Locations implements JsonSerializable
+{
+    /**
+     * @var \BmgApiV2Lib\Models\Location[] $data
+     */
+    public $data;
+
+    /**
+     * Constructor to set initial or default values of member properties
+     * @param array $data Initialization value for $this->data
+     */
+    public function __construct()
+    {
+        if (1 == func_num_args()) {
+            $this->data = func_get_arg(0);
+        }
+    }
+
+
+    /**
+     * Encode this object to JSON
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        $json = array();
+        $json['data'] = $this->data;
+
+        return $json;
+    }
+}
