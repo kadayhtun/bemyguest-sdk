f05af58ac99ebf07d6404a6718859e7bb19d8e2e
Chitmin <aungchitmin@owaytrip.com>
2018-08-21 10:43:20 +0630

    add more utils

---
+++ tests/Utils/LocationResolverTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BmgApiV2Lib\Tests\Utils;
+
+use BmgApiV2Lib\Models\City;
+use BmgApiV2Lib\BmgApiV2Client;
+use BmgApiV2Lib\Tests\TestCase;
+use BmgApiV2Lib\Utils\LocationResolver;
+
+class LocationResolverTest extends TestCase
+{
+    public function testFlatten()
+    {
+        $locations = (new BmgApiV2Client)
+            ->getClient()
+            ->getConfig()
+            ->data
+            ->locations
+            ->data;
+
+        $resolved = LocationResolver::resolve($locations, 'cities');
+
+        $this->assertContainsOnlyInstancesOf(
+            City::class,
+            $resolved
+        );
+    }
+}
